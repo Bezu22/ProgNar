04dffe6209c67e7f46b450714cb5ec5fd45a59d5
Bezu22 <tkosiarski@yahoo.com>
2025-08-28 13:17:45 +0200

    dodano funkcje rabatowania , nie skonczone

--- ProgNar/core/notes_main.py
+++ ProgNar/core/notes_main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 
 class NotesMenu:
@@ -17,6 +18,21 @@ class NotesMenu:
         self.cart = cart
         self.item_index = item_index
         self.main_app = main_app
+        #POBIERZ STAN RABATU Z KOSZYKA
+        self.discount_status = "-"  # Do pobrania z cart
+
+        if self.discount_status != "-":
+            pass
+        else:
+            self.discount_check_var = tk.BooleanVar(value = False)
+            self.discount_value = tk.StringVar(value='0')
+
+        self.discount_options = ["Szlifowanie", "Powłoka"]
+        self.discount_value = tk.StringVar(value = '0')
+
+        #---
+
+        self.discount_option_var = tk.StringVar(value="BRAK")
 
         # Tworzenie okna
         self.window = tk.Toplevel(parent)
@@ -43,33 +59,39 @@ class NotesMenu:
         ).pack(anchor="nw", pady=5)
 
         # Pole tekstowe na uwagi (3 linie)
-        self.remarks_text = tk.Text(self.main_frame, height=3, width=40, font=("Arial", 10))
-        self.remarks_text.pack(pady=5, fill=tk.BOTH, expand=True)
+        self.remarks_text = tk.Text(self.main_frame, height=1, width=40, font=("Arial", 10),relief='solid', bd=2)
+        self.remarks_text.pack(pady=5, fill=tk.BOTH)
         if current_remarks != '-':
             self.remarks_text.insert(tk.END, current_remarks)
-            self.remarks_text.config(state='normal', bg="white")
+            self.remarks_text.config(state='normal', bg="white",height=3)
         else:
             self.remarks_text.config(state='disabled', bg="#d3d3d3")
+        #Rabatowanie
+        self.rabaty_frame()
 
-        # Przycisk zapisu
+
+        #, relief='solid', bd=2
+        # Przyciski zapisu i wyjscia
+        self.quit_buttons_frame = tk.Frame(self.main_frame)
+        self.quit_buttons_frame.pack(pady=5)
         tk.Button(
-            self.main_frame,
+            self.quit_buttons_frame,
             text="Zapisz",
             command=self.save_remarks,
             font=("Arial", 12),
             bg="#4CAF50",
             fg="white"
-        ).pack(pady=10)
+        ).pack(pady=10,side='left')
 
         # Przycisk anulowania
         tk.Button(
-            self.main_frame,
+            self.quit_buttons_frame,
             text="Anuluj",
             command=self.window.destroy,
             font=("Arial", 12),
             bg="#f44336",
             fg="white"
-        ).pack(pady=5)
+        ).pack(pady=15,padx=20,side='left')
 
     def center_window(self, window, width=400, height=300):
         window.update_idletasks()  # Upewnia się, że rozmiary są aktualne
@@ -85,11 +107,11 @@ class NotesMenu:
     def toggle_remarks_field(self):
         """Włącza/wyłącza pole tekstowe na podstawie stanu checkboxa."""
         if self.remarks_active_var.get():
-            self.remarks_text.config(state='normal', bg="white")
+            self.remarks_text.config(state='normal', bg="white",height=3)
             self.remarks_text.focus_set()
         else:
             self.remarks_text.delete("1.0", tk.END)
-            self.remarks_text.config(state='disabled', bg="#d3d3d3")
+            self.remarks_text.config(state='disabled', bg="#d3d3d3",height=1)
 
     def save_remarks(self):
         """Zapisuje uwagi do pozycji w koszyku i aktualizuje widok."""
@@ -112,3 +134,52 @@ class NotesMenu:
         self.cart.update_cart_display(self.main_app.cart_tree)
 
         self.window.destroy()
+
+    def rabaty_frame(self):
+        #glowny frame
+        self.rabaty_mainframe = tk.Frame(self.main_frame,relief='solid', bd=2)
+        self.rabaty_mainframe.pack(pady = 5,anchor = 'w')
+        #checkbox
+
+        tk.Checkbutton(
+            self.rabaty_mainframe,
+            text="Dodaj rabat",
+            variable=self.discount_check_var,
+            command=self.toggle_discount_check,
+            font=("Arial", 12),
+            bg="#f0f0f0"
+        ).pack(pady=1,anchor="nw")
+
+        #discount options
+        self.discount_options_frame = tk.Frame(self.rabaty_mainframe)
+        #comboboxy
+
+        self.left_discount_frame = tk.Frame(self.discount_options_frame,relief='solid', bd=2)
+        self.left_discount_frame.pack()
+        self.discount_options = ["Szlifowanie" , "Powłoka"]
+        self.discount_options_combo = ttk.Combobox(self.left_discount_frame, state="readonly",
+                                         width=10)
+        self.discount_options_combo["values"] = self.discount_options
+        self.discount_options_combo.current(0)
+        self.discount_options_combo.pack(pady=2,side = 'left')
+        #self.discount_option_combo.bind("<<ComboboxSelected>>", self.empty)
+
+        self.discount_value_entry = tk.Entry(self.left_discount_frame,textvariable=self.discount_value,width=8)
+        self.discount_value_entry.pack(pady=2,padx=10,side = 'left')
+
+        #kwota
+        self.cena_frame = tk.Frame(self.main_frame)
+        self.cena_frame.pack(pady = 5,anchor = 'w')
+
+
+    def toggle_discount_check(self):
+        if self.discount_check_var.get() == True:
+            self.discount_options_frame.pack(pady=2)
+
+        else:
+            self.discount_status = "-"
+            self.discount_value = "0"
+            self.discount_options_frame.pack_forget()
+
+    def empty(self):
+        pass
